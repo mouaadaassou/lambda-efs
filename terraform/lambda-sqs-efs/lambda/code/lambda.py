54d5874509abd7d15d9fee58eda6c5635840198f
Moaad <aassou.mouad@gmail.com>
2023-01-29 14:15:55 +0100

    remocing a trailing dash from lambda function code

--- terraform/lambda-sqs-efs/lambda/code/lambda.py
+++ terraform/lambda-sqs-efs/lambda/code/lambda.py
@@ -3,7 +3,7 @@ import datetime
 
 def lambda_handler(event, context):
     now = datetime.datetime.now()
-    filename = "/mnt/lambda/filename-" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-") + ".txt"
+    filename = "/mnt/lambda/filename-" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
     file = open(filename, "x")
     file.write("Content from Lambda function, at " + now.strftime("%d/%m/%Y, %H:%M:%S"))
 
